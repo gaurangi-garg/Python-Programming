e0bee6586ddb13392dcbf11c61b9cecfc157146e
gaurangi-garg <gaurangi293@gmail.com>
2026-02-02 16:46:23 +0530

    Create 04_type_casting.py

---
+++ 01_Basics/04_type_casting.py
@@ -0,0 +1,8 @@
+# Type Casting
+age_str=input("Enter your age:")
+age_int=int(age_str)
+age_float=float(age_str)
+print("As int:",age_int)
+print("Type of age_int is",type(age_int))
+print("As float:",age_float)
+print("Type of age_float is",type(age_float))
