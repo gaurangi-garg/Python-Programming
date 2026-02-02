a5ed10c08e6ce274220cc7349b3e275cda47d2d1
gaurangi-garg <gaurangi293@gmail.com>
2026-02-02 17:02:32 +0530

    Create 06_comparison_operators.py

---
+++ 01_Basics/06_comparison_operators.py
@@ -0,0 +1,7 @@
+# Comparison operators
+num1=int(input("Enter First number:"))
+num2=int(input("Enter second number:"))
+print("num1==num2",num1==num2)
+print("num1!=num2",num1!=num2)
+print("num1<=num2",num1<=num2)
+print("num>=num2",num1>=num2)
