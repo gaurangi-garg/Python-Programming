8596ff89543e4345642a6eb5ab6d9cbd1b7ae542
gaurangi-garg <gaurangi293@gmail.com>
2026-02-02 16:53:12 +0530

    Create 05_airthmetic_operations.py

---
+++ 01_Basics/05_airthmetic_operations.py
@@ -0,0 +1,7 @@
+# Airthmetic operations
+num1=int(input("Enter first number:"))
+num2=int(input("Enter another number:"))
+print("Sum of",num1,"and",num2,"is",num1+num2)
+print("Difference of",num1,"and",num2,"is",num1-num2)
+print("Product of",num1,"and",num2,"is",num1*num2)
+print("Division of",num1,"and",num2,"is",num1/num2)
