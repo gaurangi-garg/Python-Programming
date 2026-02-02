02bd04693229bda46f526d20e0edb9930a05bc2e
gaurangi-garg <gaurangi293@gmail.com>
2026-02-02 17:17:02 +0530

    Create 08_assignment_operators.py

---
+++ 01_Basics/08_assignment_operators.py
@@ -0,0 +1,11 @@
+# Assignment operators
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+x+=y
+print("x+=y:",x)
+x-=y
+print("x-=y:",x)
+x*=y
+print("x*=y:",x)
+x/=y
+print("x/=y:",x)
