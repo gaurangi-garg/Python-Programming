3f6e6f515316c844bfc8d70e4771b1f6fea1a5b2
gaurangi-garg <gaurangi293@gmail.com>
2026-02-05 17:36:09 +0530

    Update 14_String operations.py

--- 01_Basics/14_String operations.py
+++ 01_Basics/14_String operations.py
@@ -1,1 +1,6 @@
 # String operations
+str1=input("Enter string1:")
+str2=input("Enter string2:")
+print("Concatentaion:",str1+str2)
+print("Repetitive:","\n","string1 4 times",str1*4)
+print("slicing str1[0:3]:",str1[0:3])
